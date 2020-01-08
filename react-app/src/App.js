30f322645e59ef0a2bc9eedaa1facd77f31d63ab
Jordan McCollam <jordy.mccollam@gmail.com>
2020-01-07 22:12:11 -0600

    Added login and signup pages / no function yet

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -4,8 +4,10 @@ import './App.css';
 import './css/animation.css';
 // import './css/bootstrap-lumen.css';
 import './css/style.css';
-import Navbar from "./components/Navbar"
-import GiftBubble from "./components/GiftBubble"
+import Navbar from "./components/Navbar";
+import GiftBubble from "./components/GiftBubble";
+import Login from "./components/Login";
+import SignUp from "./components/SignUp";
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
       <Navbar/>
       <div className="container-fluid">
         <GiftBubble/>
+        {/* <Login/> */}
+        {/* <SignUp/> */}
       </div>
     </div>
   );
---
+++ react-app/src/components/SignUp/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+function SignUp() {
+    return(
+        <div>
+            <div className="row justify-content-center font-custom">
+                <div className="col-lg-4 text-left">
+                    <h1 className="display-3 text-center">S I G N U P</h1>
+                    <form>
+                        <div className="form-group">
+                            <input className="form-control" type="text" placeholder="username"/>
+                        </div>
+                        <div className="form-group">
+                            <input className="form-control" type="password" placeholder="password"/>
+                        </div>
+                        <button className="btn btn-block btn-lg btn-primary">Submit</button>
+                        <button className="btn btn-block btn-secondary"><i class="fas fa-caret-left"></i> Back</button>
+                    </form>
+                    <a href="#">Login</a>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default SignUp;

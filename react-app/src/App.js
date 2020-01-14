2a092c4834bfe05e87ee115b1ca1993bc6ce1070
Jordan McCollam <jordy.mccollam@gmail.com>
2020-01-13 21:17:01 -0600

    Dynamic navbar / based on logged in status

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import API from "./utils/API";
 import './App.css';
 import './css/animation.css';
 import './css/style.css';
@@ -9,22 +10,52 @@ import SignUp from "./components/SignUp";
 import Home from "./Pages/Home";
 import Profile from "./Pages/Profile";
 
-function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Navbar/>
-        <Switch>
-          <div className="container-fluid">
-            <Route exact path="/" component={Home} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/signup" component={SignUp} />
-            <Route exact path="/profile" component={Profile} />
-          </div>
-        </Switch>
-      </div>
-    </Router>
-  );
+class App extends React.Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      user: {}
+    }
+  }
+
+  componentDidMount() {
+    API.getAuthStatus()
+        .then(res => {
+
+          console.log(res.data);
+
+          this.setState({user: res.data});
+
+        }).catch(err => console.log(err));
+  }
+
+
+
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          {/* <Navbar/> */}
+          <Switch>
+            <div className="container-fluid">
+              <Route 
+              exact 
+              path="*" 
+              render={props => (
+                <Navbar { ... props} user={this.state.user} />
+              )} />
+              <Route exact path="/" component={Home} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/signup" component={SignUp} />
+              <Route exact path="/profile" component={Profile} />
+            </div>
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
--- react-app/src/utils/API.js
+++ react-app/src/utils/API.js
@@ -1,5 +1,11 @@
 import axios from "axios";
 
 export default {
-  
+  getAuthStatus: function() {
+
+    // return "TEST 1";
+
+    return axios.get("/api/auth/getAuthStatus");
+
+  }
 };
